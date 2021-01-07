846d5518864f71a5aee3e69f4101c5de373c28ce
kons-9 <gotoshi.euphonium@gmail.com>
2021-01-07 19:28:39 +0900

    update

--- homepage/index.js
+++ homepage/index.js
@@ -1,14 +1,16 @@
 // declare variable for Serial object
 var serial;
+var accel_id;
+var audioElm = new Audio('betty45.mp3');
 
 webiopi().ready(init);
 
 function init(){
     serial = new Serial("uno");
+    accel_id = setInterval(accel, 1000);
 }
 function work() {
     serial.write("n");
-    serial.read(callback)
     console.log("work");
 }
 
@@ -26,12 +28,29 @@ function callback(data){
     console.log(data)
 }
 
+function sleep(){
+    serial.write("s")
+}
+
 function use_kasoku(){
     serial.write("k")
+    clearInterval(accel_id);
+    accel_id = setInterval(accel,1000);
     console.log("use kasoku")
 }
 
 function unuse_kasoku(){
     serial.write("a")
+    clearInterval(accel_id);
     console.log("unuse kasoku")
+}
+
+var accel = function (){
+    serial.read(function(data){
+        console.log(data)
+        if(data=="o"){
+            console.log("play")
+            audioElm.play();
+        }
+    })
 }
